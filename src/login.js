d09d051863fb3648db00e928c38ff5c1afd8fb92
SusanDz <sd2009@hw.ac.uk>
2022-01-20 19:08:55 +0400

    initial commit

---
+++ src/login.js
@@ -0,0 +1,91 @@
+import { useState, useEffect } from "react";
+import axios from 'axios'
+
+const Login = () => {
+    const [user, setUser] = useState();
+
+    const [info, setInfo] = useState({
+        username: "",
+        password: ""
+    });
+
+    const [loggedIN, setloggedIN] = useState();
+    const [denied, setDenied] = useState();
+    const handleInfo = (event) =>{
+        const name = event.target.name;
+        const value = event.target.value;
+        setInfo({...info, [name] : value})
+    }
+
+    function getUser(){
+        axios
+        .get('http://localhost:3000/profiles')
+        .then((res) => {
+            setUser(res.data);
+        }
+        );
+    }
+
+    const handleSubmit = (event) =>{
+        event.preventDefault();
+        getUser();
+        
+    }
+
+    useEffect(() => {
+        {user && traverseData()}
+    }, [user]);
+
+    function traverseData() {
+        var i
+        for(i = 0; i<user.length; i++) {
+            if(user[i].username === info.username) {
+                if(user[i].password === info.password) {
+                    console.log('HI', user[i].username);
+                    setloggedIN(true);
+                    setDenied(false);
+                    // console.log(loggedIN)
+                    // console.log(denied)
+                    break;
+                }
+                else{
+                    setDenied(true)
+                }
+            } else {
+                setDenied(true)
+            }
+        }
+
+        if(loggedIN === false) {
+            setDenied(true);
+            console.log(denied);
+        }
+    }
+// to do
+// sign up - make sure password is fllowing a cirtain pattern
+// if confirm is not equal show page
+    return ( 
+        <div className="container" >
+            <div className="form">
+            <div className="header">Login</div>
+               {loggedIN ? <div className="sub">tag to home page in actual application</div> : null} {/* instead of the div we gonna link profile page so add <profile></profile> */}
+               {/* {loggedIN ? <div className="errorHandle">loggged in</div> : null}*/}
+               {denied ? <div className="errorHandle">denied</div> : null} 
+               
+                <div className="form_group">
+                    <label htmlFor="Username">Username</label>
+                    <input onChange={handleInfo} value={info.username} type="text" name="username" placeholder="Username"></input>                    
+                </div>
+                <div className="form_group">
+                    <label htmlFor="Password">Password</label>
+                    <input onChange={handleInfo} value={info.password} type="password" name="password" placeholder="Password"></input>
+                </div>
+                <div className="footer">
+            <button onClick={handleSubmit} type="button" className="b1">Login</button>
+            </div>
+            </div>
+        </div>
+     );
+}
+ 
+export default Login;
---
+++ src/signup.js
@@ -0,0 +1,99 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import {v4 as uuidv4} from 'uuid';
+
+const SignUp = () => {
+    const [fName, setFName] = useState('');
+    const [lName, setLName] = useState('');
+    const [email, setEmail] = useState('');
+    const [username, setUsername] = useState('');
+    const [confirm, setConfirmPassword] = useState('');
+    const [password, setPassword] = useState('');
+    const [bio, setBio] = useState('');
+
+    const[user, setUser] = useState()
+    const [PasswordDiff, setPasswordDiff] = useState();
+
+    function postUser(fName,lName,email,username,password,bio){
+        axios
+        .post('http://localhost:3000/profiles', {
+            id: uuidv4(),
+            fName: fName,
+            lName: lName,
+            email: email,
+            username: username,
+            password: password,
+            bio: bio
+        })
+        .then((res) => {
+          setUser(res.data);
+        });
+    }
+
+    useEffect(() => {
+        console.log(user);
+    },[user]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if(confirm === password) {
+            postUser(fName, lName, email, username, password, bio);
+            setPasswordDiff(false);
+        } else {
+            setPasswordDiff(true);
+        }
+    }
+
+    return ( 
+        <div className="container">
+            <div className="form">
+            <div className="header">Create an account</div>
+            {PasswordDiff ? <div className="errorHandle">Password is not the same try again</div> : null}
+                <div className="form_group">
+                    <label htmlFor="First Name">Name</label>
+                    <input type="text" name="firstname" placeholder="First Name" value={fName} onChange={(e) => setFName(e.target.value)} className="name_textBox"></input>
+                    
+                    <input type="text" name="lastname" placeholder="Last Name" value={lName} onChange={(e) => setLName(e.target.value)} className="name_textBox"></input>
+                </div>
+                <div className="form_group">
+                    Email
+                    <br />
+                    <input type="text" name="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)}></input>
+                </div>
+                <div className="form_group">
+                    Username
+                    <br />
+                    <input type="text" name="username" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)}></input>
+                </div>
+                <div className="form_group">
+                    Password
+                    <br />
+                    <input type="password" name="password" placeholder="Password" value={password}  onChange={(e) => setPassword(e.target.value)}></input>
+                </div>
+                <div className="form_group">
+                    Confirm Password
+                    <br />
+                    <input type="password" name="Changepassword" placeholder="Password"  onChange={(e) => setConfirmPassword(e.target.value)}></input>
+                </div>
+                <div className="form_group">
+                <div className="pfp">
+                    <h3>Add Profile Picture</h3>
+
+                    {/* need to add the functionallity for uploading poic ture will do later after finishing the resty of the work */}
+
+                </div>
+                </div>
+                <div className="form_group">
+                    Add Biography
+                    <br />
+                    <input type="text" name="bio" placeholder="Bio..." value={bio} onChange={(e) => setBio(e.target.value)}></input>
+                </div>
+                <div className="footer">
+            <button type="button" onClick={handleSubmit} className="b1">LOGIN</button>
+        </div>
+            </div>
+        </div>
+     );
+}
+ 
+export default SignUp;
