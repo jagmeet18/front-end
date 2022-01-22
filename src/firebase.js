8cf575e32ce4643504224bff897366a7c0efcd4e
SusanDz <sd2009@hw.ac.uk>
2022-01-22 21:43:41 +0400

    firbase config file

--- src/firebase.js
+++ src/firebase.js
@@ -1,34 +1,20 @@
-import { createContext } from 'react';
-// import 'firebase-admin/firestore';
-import firebase from 'firebase'
-import SERVICE_ACCOUNT_KEY from './keys/SERVICE_ACCOUNT_KEY'
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-const firebaseApp = firebase.initializeApp(
-	{
-		credential: firebase.credential.cert(SERVICE_ACCOUNT_KEY),
-	},
-	"insights-server"
-)
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAbzQ9KWdzIgc7WQBYBcwNPVVlfJJ62o_Q",
+    authDomain: "insights-server-fd594.firebaseapp.com",
+    projectId: "insights-server-fd594",
+    storageBucket: "insights-server-fd594.appspot.com",
+    messagingSenderId: "296327912616",
+    appId: "1:296327912616:web:23a251ebb954f7473ef69d",
+    measurementId: "G-DTD0FKZHPM"
+};
 
-// import {initializeApp} from "firebase-admin/app";
-
-const FirebaseContext = createContext(null)
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAbzQ9KWdzIgc7WQBYBcwNPVVlfJJ62o_Q",
-//   authDomain: "insights-server-fd594.firebaseapp.com",
-//   projectId: "insights-server-fd594",
-//   storageBucket: "insights-server-fd594.appspot.com",
-//   messagingSenderId: "296327912616",
-//   appId: "1:296327912616:web:23a251ebb954f7473ef69d",
-//   measurementId: "G-DTD0FKZHPM"
-// };
-
-// const firebase = initializeApp(firebaseConfig);
-
-// const { FieldValue } = firebase.firestore;
-
-const db = firebase.firestore(firebaseApp)
-
-export { db };
-export default FirebaseContext;
+// Initialize Firebase
+initializeApp(firebaseConfig);
+const db = getFirestore();
+export default db
